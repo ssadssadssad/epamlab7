e6a3c1c2ca6754cf1a7650691f12148bb55374ec
Maksim <70893567+ssadssadssad@users.noreply.github.com>
2021-11-10 17:03:23 +0300

    Add files via upload

---
+++ Net/Aircompany/Airport.cs
@@ -0,0 +1,52 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        public IEnumerable<Plane> planes { get; private set; }
+
+        public Airport(IEnumerable<Plane> planes)
+        {
+            this.planes = planes;
+        }
+
+        public IEnumerable<T> GetPlanes<T>() where T : Plane
+        {
+            return planes.Where(plane => plane.GetType() == typeof(T)).Cast<T>();
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            return GetPlanes<PassengerPlane>().OrderByDescending(plane => plane.passengersCapacity).First();
+        }
+
+        public IEnumerable<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            return GetPlanes<MilitaryPlane>().Where(plane => plane.militaryType == MilitaryType.TRANSPORT);
+        }
+
+        public Airport SortByMaxDistance()
+        {
+            return new Airport(planes.OrderBy(plane => plane.maxFlightDistance));
+        }
+
+        public Airport SortByMaxSpeed()
+        {
+            return new Airport(planes.OrderBy(plane => plane.maxSpeed));
+        }
+
+        public Airport SortByMaxLoadCapacity()
+        {
+            return new Airport(planes.OrderBy(plane => plane.maxLoadCapacity));
+        }
+
+        public override string ToString()
+        {
+            return $"Airport planes: {string.Join(",  ", planes.Select(plane => plane.model))}";
+        }
+    }
+}
---
+++ Net/Aircompany/Planes/Plane.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Aircompany.Planes
+{
+    public abstract class Plane
+    {
+        public string model { get; private set; }
+        public int maxSpeed { get; private set; }
+        public int maxFlightDistance { get; private set; }
+        public int maxLoadCapacity { get; private set; }
+
+        public Plane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity)
+        {
+            this.model = model;
+            this.maxSpeed = maxSpeed;
+            this.maxFlightDistance = maxFlightDistance;
+            this.maxLoadCapacity = maxLoadCapacity;
+        }
+
+        public override string ToString()
+        {
+            return $"Plane( Model = {model}, MaxSpeed = {maxSpeed}, MaxFlightDistance = {maxFlightDistance}, MaxLoadCapacity = {maxLoadCapacity})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Plane plane = obj as Plane;
+            return plane != null &&
+                   model == plane.model &&
+                   maxSpeed == plane.maxSpeed &&
+                   maxFlightDistance == plane.maxFlightDistance &&
+                   maxLoadCapacity == plane.maxLoadCapacity;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1043886837;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(model);
+            hashCode = hashCode * -1521134295 + maxSpeed.GetHashCode();
+            hashCode = hashCode * -1521134295 + maxFlightDistance.GetHashCode();
+            hashCode = hashCode * -1521134295 + maxLoadCapacity.GetHashCode();
+            return hashCode;
+        }
+
+    }
+}
